a16914d409fc9d08d4af69068e26b8622c2564f9
Sviatlana Tamashevich <tamashevichs@gmail.com>
2023-04-10 19:48:01 +0300

    Adding Task_1 and Task_2

---
+++ Task_2/tests/Task_2.spec.js
@@ -0,0 +1,52 @@
+import { test, expect } from "@playwright/test";
+
+test("filling the placeholder", async ({ page }) => {
+  await page.goto("https://demoqa.com/books");
+  await page.getByPlaceholder("Type to search").fill("Git");
+  await page.locator("#basic-addon2 svg").click();
+  await expect(page).toHaveURL("https://demoqa.com/books");
+});
+
+test("filling the placeholder2", async ({ page }) => {
+  await page.goto("https://www.toolsqa.com/selenium-training/");
+  await page.getByPlaceholder("Search").nth(1).fill("Selenium");
+  await page.getByPlaceholder("Search").nth(1).press("Enter");
+  await expect(page).toHaveURL(
+    "https://www.toolsqa.com/search?keyword=Selenium"
+  );
+});
+
+test("Check the checkbox", async ({ page }) => {
+  await page.goto("https://demoqa.com/automation-practice-form");
+  await page.getByText("Sports").click();
+  expect(await page.getByText("Sports").isChecked()).toBeTruthy;
+});
+
+test("hover", async ({ page }) => {
+  await page.goto("https://demoqa.com/menu#");
+  await page.getByRole("link", { name: "Main Item 2" }).hover();
+});
+
+test("drag & drop ", async ({ page }) => {
+  await page.goto("https://demoqa.com/droppable");
+  await page
+    .locator("#draggable.drag-box.mt-4.ui-draggable.ui-draggable-handle")
+    .dragTo(
+      page.locator("#simpleDropContainer #droppable.drop-box.ui-droppable")
+    );
+});
+
+test("Upload one file", async ({ page }) => {
+  await page.goto("https://demoqa.com/upload-download");
+  await page.getByLabel("Select a file").setInputFiles("myFile.pdf");
+  const path = page.locator("#uploadedFilePath");
+  await expect(path).toContainText("myFile.pdf");
+});
+
+test("Select options", async ({ page }) => {
+  await page.goto("https://demoqa.com/select-menu");
+  await page.locator("#oldSelectMenu").selectOption("8");
+  await expect(page.locator("#oldSelectMenu option:checked")).toHaveText(
+    " Indigo"
+  );
+});
