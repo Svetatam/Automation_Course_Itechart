ac881d2f6824f0219cdb522dacbafec3598e7dde
Sviatlana Tamashevich <tamashevichs@gmail.com>
2023-10-04 16:04:51 +0300

    task 6

--- Task_6/utils/downloadUtil.ts
+++ Task_6/utils/downloadUtil.ts
@@ -1,10 +1,4 @@
 import { Page } from '@playwright/test'
-<<<<<<< HEAD
-
-=======
-import fs from 'fs'
-import path from 'path'
->>>>>>> 7dce5be30291f173cf9069572a500689c2f9aa9d
 
 export class DownloadUtil {
   private page: Page
@@ -23,7 +17,6 @@ export class DownloadUtil {
       .click()
 
     const download = await downloadPromise
-<<<<<<< HEAD
     const path = require('path')
 
     // Полный путь к папке userFiles
@@ -34,20 +27,6 @@ export class DownloadUtil {
 
     // Полный путь к файлу
     const savePath = path.join(userFilesPath, fileName)
-=======
-    const path = require('path');
-
-      // Полный путь к папке userFiles
-      const userFilesPath = path.resolve(__dirname, '../userFiles');
-      
-      // Генерировать имя файла на основе текущей даты и времени
-      const fileName = 'at_' + Date.now() + '.exe';
-      
-      // Полный путь к файлу
-      const savePath = path.join(userFilesPath, fileName);
-
-
->>>>>>> 7dce5be30291f173cf9069572a500689c2f9aa9d
 
     await download.saveAs(savePath)
   }
